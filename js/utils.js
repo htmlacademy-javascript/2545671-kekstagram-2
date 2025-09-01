29850559a5bed11263f7b12b1c3ed4f7e52b9bb0
Леночка <leno4kagaripova@yandex.ru>
2025-09-01 16:30:24 +0700

    Добавлен дополнительный модуль

--- js/utils.js
+++ js/utils.js
@@ -1,8 +1,7 @@
 const getRandomInteger = (a, b) => {
   const lower = Math.ceil(Math.min(a, b));
   const upper = Math.floor(Math.max(a, b));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
+  return Math.random() * (upper - lower + 1) + lower;
 };
 const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
 
